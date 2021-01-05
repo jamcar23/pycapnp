23d0859da87b7fb57f0177cbb6f953d972ea5a7e
James Carroll <jamcar23@gmail.com>
2021-01-05 11:25:55 -0600

    feat(cli): improved op build script to be more complete

--- build_for_op.sh
+++ build_for_op.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/bash
 
+echo "Installing requirements..."
+pip install -r requirements.txt
+echo "Installing requirements done."
+
+echo "Build pycapnp..."
+python setup.py build --force-cython --force-setup-libcapnp
+echo "Build pycapnp done."
+
+echo "Build bdist..."
+python setup.py bdist_wheel --force-cython --force-setup-libcapnp
+echo "Build bdist done."
+
+echo "Build sdist..."
+python setup.py sdist --force-cython --force-setup-libcapnp
+echo "Build sdist done."
+
+echo "Install pycapnp..."
 python setup.py install --force-cython --force-system-libcapnp
+echo "Install pycapnp done."
+
